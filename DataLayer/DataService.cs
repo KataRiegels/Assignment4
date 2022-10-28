8255859efa4a22e1becf2f250a066e093d7bd44c
KataRiegels <62016188+KataRiegels@users.noreply.github.com>
2022-10-28 18:54:55 +0200

    Add project files.

---
+++ DataLayer/DataService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataLayer.Model;
+
+namespace DataLayer
+{
+    public class DataService
+    {
+        public IList<Category> GetCategories() 
+        {
+            using var db = new NorthwindContext();
+            return db.Categories.ToList();
+        }
+
+        public Category GetCategory(int id)
+        {
+            using var db = new NorthwindContext();
+            return db.Categories.FirstOrDefault(x => x.Id == id);
+ 
+        }
+
+        public Category CreateCategory(string name, string description)
+        {
+            Category category = new Category();
+            using var db = new NorthwindContext();
+            var maxId = db.Categories.Max(x => x.Id);
+            category.Id = maxId + 1;
+            category.Name = name;
+            category.Description = description;
+            db.Categories.Add(category);
+            return category;
+        }
+
+        public bool DeleteCategory(int id)
+        {
+            var db = new NorthwindContext();
+            var category = GetCategory(id);
+            if (category == null)
+            {
+                return false;
+            }
+            db.Categories.Remove(GetCategory(id));
+
+            return true;
+        }
+    }
+}
---
+++ DataLayer/NorthwindContext.cs
@@ -0,0 +1,51 @@
+﻿using DataLayer.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public class NorthwindContext : DbContext
+    {
+        const string ConnectionString = "host=localhost;db=northwind;uid=postgres;pwd=Jse33pjp";
+
+        public IList<Category> Categories { get; set; }
+        public IList<Product> Products { get; set; }
+        public IList<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            //optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+            optionsBuilder.UseNpgsql(ConnectionString);
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().ToTable("categories");
+            modelBuilder.Entity<Category>().Property(x => x.Id).HasColumnName("categoryid");
+            modelBuilder.Entity<Category>().Property(x => x.Name).HasColumnName("categoryname");
+            modelBuilder.Entity<Category>().Property(x => x.Description).HasColumnName("description");
+
+            modelBuilder.Entity<Product>().ToTable("products");
+            modelBuilder.Entity<Product>().Property(x => x.Id).HasColumnName("productid");
+            modelBuilder.Entity<Product>().Property(x => x.Name).HasColumnName("productname");
+            modelBuilder.Entity<Product>().Property(x => x.CategoryId).HasColumnName("categoryid");
+
+            modelBuilder.Entity<Order>().ToTable("orders");
+            modelBuilder.Entity<Order>().Property(x => x.Id).HasColumnName("orderid");
+            modelBuilder.Entity<Order>().Property(x => x.customerid).HasColumnName("customerid");
+            modelBuilder.Entity<Order>().Property(x => x.orderdate).HasColumnName("orderdate");
+            modelBuilder.Entity<Order>().Property(x => x.requireddate).HasColumnName("requireddate");
+            modelBuilder.Entity<Order>().Property(x => x.shippeddate).HasColumnName("shippeddate");
+            modelBuilder.Entity<Order>().Property(x => x.freight).HasColumnName("freight");
+            modelBuilder.Entity<Order>().Property(x => x.shipname).HasColumnName("shipname");
+            modelBuilder.Entity<Order>().Property(x => x.shipaddress).HasColumnName("shipaddress");
+            modelBuilder.Entity<Order>().Property(x => x.shipcity).HasColumnName("shipcity");
+            modelBuilder.Entity<Order>().Property(x => x.shippostalcode).HasColumnName("shippostalcode");
+            modelBuilder.Entity<Order>().Property(x => x.shipcountry).HasColumnName("shipcountry");
+
+        }
+    }
+}
